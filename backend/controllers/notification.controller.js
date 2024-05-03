041f92368e26b0af32cb9515a137db88801fa31e
Iskander Akhmedov <ais234uz@gmail.com>
2024-05-03 21:38:31 +0500

    3- Notification query and mutation added

--- backend/controllers/notification.controller.js
+++ backend/controllers/notification.controller.js
@@ -1,21 +1,22 @@
 import Notification from "../models/notification.model.js"
 
 export const getNotifications = async (req, res) => {
-    try {
-        const userId = req.user._id
+	try {
+		const userId = req.user._id;
 
-        const notifications = await Notification.find({ to: userId }).populate({
-            path: 'from',
-            select: 'username profileImg'
-        })
-        await Notification.updateMany({ to: userId }, { read: true })
+		const notifications = await Notification.find({ to: userId }).populate({
+			path: "from",
+			select: "username profileImg",
+		});
 
-        res.status(200).json(notifications)
-    } catch (error) {
-        console.log("Error getting notifications", error)
-        res.status(500).json("Server error");
-    }
-}
+		await Notification.updateMany({ to: userId }, { read: true });
+
+		res.status(200).json(notifications);
+	} catch (error) {
+		console.log("Error in getNotifications function", error.message);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
+};
 export const deleteNotifications = async (req, res) => {
     try {
         const userId = req.user._id
@@ -29,25 +30,25 @@ export const deleteNotifications = async (req, res) => {
         res.status(500).json("Server error");
     }
 }
-// export const deleteNotification = async (req, res) => {
-//     try {
-//         const userId = req.user._id;
-//         const notificationId = req.params.id;
+export const deleteNotification = async (req, res) => {
+    try {
+        const userId = req.user._id;
+        const notificationId = req.params.id;
         
-//         const notification = await Notification.findById(notificationId);
+        const notification = await Notification.findById(notificationId);
 
-//         if (!notification) {
-//             return res.status(404).json("Notification not found");
-//         }
+        if (!notification) {
+            return res.status(404).json("Notification not found");
+        }
 
-//         if (notification.to.toString() !== userId.toString()) {
-//             return res.status(403).json("Unauthorized");
-//         }
+        if (notification.to.toString() !== userId.toString()) {
+            return res.status(403).json("Unauthorized");
+        }
 
-//         await Notification.findByIdAndDelete(notificationId);
-//         res.status(200).json({ message: "Deleted successfully" });
-//     } catch (error) {
-//         console.log("Error deleting notification", error.message);
-//         res.status(500).json({ error: "Server error" });
-//     }
-// };
+        await Notification.findByIdAndDelete(notificationId);
+        res.status(200).json({ message: "Deleted successfully" });
+    } catch (error) {
+        console.log("Error deleting notification", error.message);
+        res.status(500).json({ error: "Server error" });
+    }
+};
