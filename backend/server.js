90fb511f6f615d208b3f97fec07dab9cc17610cd
Iskander Akhmedov <ais234uz@gmail.com>
2024-05-03 18:20:58 +0500

    3- payload limit updated in server.js

--- backend/server.js
+++ backend/server.js
@@ -19,8 +19,8 @@ cloudinary.config({
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
 app.use(cookieParser()); 
+app.use(express.json({limit: "6mb"}));
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/auth", authRoutes);
